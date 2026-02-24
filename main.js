e61cd23e430c31abdf620fd773003822cacc3e94
Arcus286 <69086207+Arcus286@users.noreply.github.com>
2026-02-24 14:12:50 +0530

    Solo Project done the project is done but i do need to refractor my code since it does have a lot of repeated code

---
+++ main.js
@@ -0,0 +1,29 @@
+function home_add1(){
+    document.getElementById("home_score").textContent++;
+}
+
+function home_add2(){
+    document.getElementById("home_score").textContent++;
+    document.getElementById("home_score").textContent++;
+}
+
+function home_add3(){
+    document.getElementById("home_score").textContent++;
+    document.getElementById("home_score").textContent++;
+    document.getElementById("home_score").textContent++;
+}
+
+function guest_add1(){
+    document.getElementById("guest_score").textContent++;
+}
+
+function guest_add2(){
+    document.getElementById("guest_score").textContent++;
+    document.getElementById("guest_score").textContent++;
+}
+
+function guest_add3(){
+    document.getElementById("guest_score").textContent++;
+    document.getElementById("guest_score").textContent++;
+    document.getElementById("guest_score").textContent++;
+}
